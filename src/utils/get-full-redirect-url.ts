c927be9a5a509dc76000fb431d47e50239147671
muhfaris <devmuhfaris@gmail.com>
2023-11-08 06:55:18 +0700

    fix: just try to fix window

--- src/utils/get-full-redirect-url.ts
+++ src/utils/get-full-redirect-url.ts
@@ -1,5 +1,10 @@
 export default function getFullRedirectUrl(redirectPath: string) {
   if (redirectPath.startsWith("http")) return redirectPath;
-  const baseUrl = process.env.URL || window.location.origin;
+
+  let baseUrl = process.env.URL;
+  if (typeof (window !== "undefined") && baseUrl == "") {
+    baseUrl = window.location.origin;
+  }
+
   return [baseUrl, redirectPath?.replace(/^\//, "")].filter(Boolean).join("/");
 }
